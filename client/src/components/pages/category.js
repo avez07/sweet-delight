a422b48df678a1834f20aeab993b42336d11a317
avez07 <ansariaavez12345@gmail.com>
2023-10-16 11:45:32 +0530

    this is updated

--- client/src/components/pages/category.js
+++ client/src/components/pages/category.js
@@ -7,7 +7,16 @@ function category() {
       <Container fluid className="category" style={{ background: "#ffefe8" }}>
         <div className="mx-4  px-4 py-3">
           <div className="text-start my-4 text-capitalize bg-light p-3 head">
-            <h3>somle cakes</h3>
+            <h3>some cakes</h3>
+          
+            <ul>
+                <li>Sort by:</li>
+                <li><a href="/#">New</a></li>
+                <li><a href="/#">recommond</a></li>
+                <li><a href="/#">price low to high</a></li>
+                <li><a href="/#">price high to low</a></li>
+
+            </ul>
           </div>
         </div>
       </Container>
