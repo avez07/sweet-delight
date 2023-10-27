a76d5cd411bbad155cf6ca878d14ed14169c45ff
avez07 <ansariaavez12345@gmail.com>
2023-10-27 13:12:49 +0530

    the admin dashboard started developing

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import {React,useState} from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./components/common/header";
@@ -11,22 +11,37 @@ import "./App.css";
 import "./style.css"
 
 function App() {
+  const [loginID, setLoginID] = useState(1);
   return (
     <>
-       <Router>
+      <Router>
       <Switch>
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={Signup} />
-
         <Route>
-          <div>
-            <Header />
-            <Switch>
-              <Route exact path="/" component={home} />
-              <Route path="/about" component={Category} />
-            </Switch>
-            <Footer />
-          </div>
+          {loginID === 1 ? (
+            <div>
+              {/* Different routes and components for login ID 1 */}
+              <Header />
+              <Switch>
+                <Route exact path="/" component={home} />
+                <Route path="/about" component={Category} />
+                {/* Add more routes specific to login ID 1 if needed */}
+              </Switch>
+              <Footer />
+            </div>
+          ) : (
+            <div>
+              
+              <Header />
+              <Switch>
+                <Route exact path="/" component={home} />
+                <Route path="/about" component={Category} />
+                {/* Add more routes for other login IDs if needed */}
+              </Switch>
+              <Footer />
+            </div>
+          )}
         </Route>
       </Switch>
     </Router>
