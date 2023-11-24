f33ffda42b1299c16607992a5ae43296fb0bd03e
avez07 <ansariaavez12345@gmail.com>
2023-11-24 13:54:46 +0530

    the pricing is completed

--- client/public/env.js
+++ client/public/env.js
@@ -1,7 +1,7 @@
 window.env = {
   "ALLUSERSPROFILE": "C:\\ProgramData",
   "APPDATA": "C:\\Users\\AgronIT\\AppData\\Roaming",
-  "CHROME_CRASHPAD_PIPE_NAME": "\\\\.\\pipe\\LOCAL\\crashpad_1116_FWIREDSWGAWCVGBW",
+  "CHROME_CRASHPAD_PIPE_NAME": "\\\\.\\pipe\\LOCAL\\crashpad_1356_FNBCXVRQIDZZANSO",
   "COLOR": "1",
   "COLORTERM": "truecolor",
   "CommonProgramFiles": "C:\\Program Files\\Common Files",
--- client/src/components/seller/add-product.js
+++ client/src/components/seller/add-product.js
@@ -1,34 +1,37 @@
-import React, { useState } from "react";
-import { Form, Button,InputGroup } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Form, Button, InputGroup } from "react-bootstrap";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css"; // import styles
 
 const BulkEdit = () => {
   const [description, setDescription] = useState("");
 
-    const [sell, setSell] = useState('');
-    const [cost, setCost] = useState('');
-    const [profit, setProfit] = useState('');
-    const [margin, setMargin] = useState('');
+  const [sell, setSell] = useState("");
+  const [cost, setCost] = useState("");
+  const [profit, setProfit] = useState("");
+  const [margin, setMargin] = useState("0.00");
+  const [gst, setGst] = useState("0.00");
+  const [Campare, setCampare] = useState("");
+  const [discount, setDiscount] = useState("0.00");
+
+
+  const calculateMargin = () => {
   
-    const handleSellChange = (e) => {
-      setSell(e.target.value);
-      calculateMargin(e.target.value, cost);
-    };
-  
-    const handleCostChange = (e) => {
-      setCost(e.target.value);
-      calculateMargin(sell, e.target.value);
-    };
-  
-    const calculateMargin = (sellValue, costValue) => {
-      const sell = parseFloat(sellValue) || 0;
-      const cost = parseFloat(costValue) || 0;
-      const calculatedProfit = sell - cost;
-      const calculatedMargin = (cost / sell)*100;
-      setMargin(calculatedMargin.toFixed(2))
-      setProfit(calculatedProfit.toFixed(2));
-    };
+    const calculatedProfit = sell - cost;
+    const calculatedMargin = (calculatedProfit / sell) * 100;
+    // const calculateGst = sell < '1000'? 5 : 12;
+    const calculateGst = 18;
+    const calculateDiscount = ((Campare - sell)/Campare)*100;
+    const finalDiscount = calculateDiscount < 0 ? 0  :calculateDiscount;
+    setMargin(calculatedMargin.toFixed(2));
+    setProfit(calculatedProfit.toFixed(2));
+    setGst(calculateGst.toFixed(2));
+    setDiscount(finalDiscount.toFixed(2));
+  };
+  useEffect(()=>{
+
+    calculateMargin();
+  })
 
   const handleDescriptionChange = (value) => {
     setDescription(value);
@@ -62,34 +65,96 @@ const BulkEdit = () => {
           />
         </Form.Group>
         <Form.Group controlId="formName" className="card my-3 p-3">
-    <Form.Label className="fw-semibold me-3">Pricing</Form.Label>
-    <div className="d-flex align-items-center my-3">
-    <InputGroup className="me-3" style={{width:'30%'}}>
-      <InputGroup.Text className="inputgroup-text">Price</InputGroup.Text>
-      <Form.Control className="add-name" type="text" onChange={handleSellChange} placeholder="0.00" />
-    </InputGroup>
-    <InputGroup style={{width:'30%'}}>
-      <InputGroup.Text className="inputgroup-text">campare Price</InputGroup.Text>
-      <Form.Control className="add-name" type="text"  placeholder="0.00" />
-    </InputGroup>
-   
-  </div>
-  <div className="d-flex align-items-center">
-    <InputGroup className="me-3" style={{width:'30%'}}>
-      <InputGroup.Text className="inputgroup-text">Cost</InputGroup.Text>
-      <Form.Control className="add-name" type="text" onChange={handleCostChange} placeholder="0.00" />
-    </InputGroup>
-    <InputGroup className="me-3" style={{width:'30%'}}>
-      <InputGroup.Text className="inputgroup-text">Profit</InputGroup.Text>
-      <Form.Control className="add-name" type="text" disabled value={profit} placeholder="0.00" />
-    </InputGroup>
-    <InputGroup style={{width:'30%'}}>
-      <InputGroup.Text className="inputgroup-text">Margin</InputGroup.Text>
-      <Form.Control className="add-name" type="text" disabled value={`${margin}%`} placeholder="0.00" />
-    </InputGroup>
-  </div>
-</Form.Group>
-
+          <Form.Label className="fw-semibold me-3">Pricing</Form.Label>
+          <div className="d-flex align-items-center my-3">
+          <InputGroup className="me-3" style={{ width: "30%" }}>
+              <InputGroup.Text className="inputgroup-text">
+                Cost
+              </InputGroup.Text>
+              <Form.Control
+                className="add-name"
+                type="text"
+                name="cost"
+                onChange={(e)=>setCost(e.target.value)}
+                placeholder="0.00"
+              />
+            </InputGroup>
+            <InputGroup className="me-3" style={{ width: "30%" }}>
+              <InputGroup.Text className="inputgroup-text">
+                Price
+              </InputGroup.Text>
+              <Form.Control
+                className="add-name"
+                type="text"
+                name="sell"
+                onChange={(e)=>setSell(e.target.value)}
+                placeholder="0.00"
+              />
+            </InputGroup>
+            <InputGroup style={{ width: "30%" }}>
+              <InputGroup.Text className="inputgroup-text">
+                Campare Price
+              </InputGroup.Text>
+              <Form.Control
+                className="add-name"
+                type="text"
+                name="campare"
+                onChange={(e)=>setCampare(e.target.value)}
+                placeholder="0.00"
+              />
+            </InputGroup>
+           
+            
+          </div>
+          <div className="d-flex align-items-center">
+          <InputGroup className="me-3" style={{ width: "25%" }}>
+              <InputGroup.Text className="inputgroup-text">
+                Discount
+              </InputGroup.Text>
+              <Form.Control
+                className="add-name"
+                type="text"
+                disabled
+                value={`${discount}%`}
+                placeholder="0.00"
+              />
+            </InputGroup>
+            <InputGroup className="me-3" style={{ width: "25%" }}>
+              <InputGroup.Text className="inputgroup-text">
+                Profit
+              </InputGroup.Text>
+              <Form.Control
+                className="add-name"
+                type="text"
+                disabled
+                value={profit}
+                placeholder="0.00"
+              />
+            </InputGroup>
+            <InputGroup className="me-3" style={{ width: "25%" }}>
+              <InputGroup.Text className="inputgroup-text">
+                Margin
+              </InputGroup.Text>
+              <Form.Control
+                className="add-name"
+                type="text"
+                disabled
+                value={`${margin}%`}
+                placeholder="0.00"
+              />
+            </InputGroup>
+            <InputGroup style={{ width: "25%" }}>
+              <InputGroup.Text className="inputgroup-text">Gst</InputGroup.Text>
+              <Form.Control
+                className="add-name"
+                type="text"
+                disabled
+                value={`${gst}%`}
+                placeholder="0.00"
+              />
+            </InputGroup>
+          </div>
+        </Form.Group>
 
         <Button variant="primary" type="submit">
           Submit
