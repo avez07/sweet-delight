9c14e2ca906ad199d8892a4b1ac84f86ed61973d
avez07 <ansariaavez12345@gmail.com>
2024-01-23 15:41:47 +0530

    this

--- server/index.js
+++ server/index.js
@@ -1,16 +1,20 @@
-import  express from "express";
-import dotenv from "dotenv";
-import router from "./live/router/web.js";
-import cors from "cors";
+import express from 'express';
+import dotenv from 'dotenv';
+import router from './live/router/web.js';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-const port = process.env.PORT;
-// app.get('/',(req,res)=>{
-//     res.json('hello world')
-// })
-app.use('/api',router);
-app.listen(port);
 
+// Use a specific route for the API
+app.use('/api', router);
+
+// Handle other routes or methods here if needed
+app.all('*', (req, res) => {
+  res.status(404).json({ message: 'Not Found' });
+});
+
+// Export the Express app for Vercel
+export default app;
